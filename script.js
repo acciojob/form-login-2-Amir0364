0a5c027312299a3adc90dbeb08816602f8ff04bb
Mohd Amir <140543590+Amir0364@users.noreply.github.com>
2024-07-06 12:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var firstName = document.getElementById('first').value;
+    var lastName = document.getElementById('last').value;
+    var phoneNumber = document.getElementById('number').value;
+    var email = document.getElementById('email').value;
+
+    alert(`First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`);
+});
